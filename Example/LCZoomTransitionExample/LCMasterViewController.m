77eda54ebfc9525acc2a3c538c4e3fead4f09cca
Michael Brown <michael@luacheia.com>
2014-01-25 11:09:24 +0000

    Tidied up license text

--- Example/LCZoomTransitionExample/LCMasterViewController.m
+++ Example/LCZoomTransitionExample/LCMasterViewController.m
@@ -1,7 +1,6 @@
-//
 /*
  
- Copyright (c) 2013 Michael Brown (me@michael-brown.net)
+ Copyright (c) 2013 Michael Brown
  
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
